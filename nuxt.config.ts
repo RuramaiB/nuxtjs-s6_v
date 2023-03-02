f2fde5a1bc056d39fc9293e10a061d08921582e1
Ruramai <95416939+RuramaiB@users.noreply.github.com>
2023-03-02 09:39:07 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,10 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
+  modules: [
+   '@nuxtjs/tailwindcss',
+  ],
+
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
